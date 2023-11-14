db91d08ae0fb7852e48bc3e08897ca7f41c81940
RafiRashya <rafi.rashya.22@gmail.com>
2023-11-14 14:01:46 +0700

    coba ubah

--- app.py
+++ app.py
@@ -3,41 +3,52 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask_login'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "flask_login"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
-app.secret_key = 'your_secret_key'
+app.secret_key = "your_secret_key"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-        return render_template('templates.html')
-        
-@app.route('/login', methods=['GET', 'POST'])
+    return render_template("Tampilan_web.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['pass']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["pass"]
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM user WHERE username = %s AND password = %s", (username, password))
+        cur.execute(
+            "SELECT * FROM user WHERE username = %s AND password = %s",
+            (username, password),
+        )
         user = cur.fetchone()
         cur.close()
         if user:
-            session['username'] = user['username']
-            return redirect(url_for('home'))
+            session["username"] = user["username"]
+            return redirect(url_for("home"))
         else:
-            return redirect(url_for('login'))
-    return render_template('index.html')
+            flash("Username or Password is incorrect", "error")
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('username', None)
-    return redirect(url_for('login'))
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
+@app.route("/search")
+def search():
+    # cari data di database berdasarkan keyword yang dicari oleh pengguna
+    pass
+
+if __name__ == "__main__":
     app.run(debug=True)
