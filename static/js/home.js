a58c4539a0df48c6f611440f4af674ef4afa5f90
ultearsativa <121283150+ultearsativa@users.noreply.github.com>
2023-11-25 02:08:29 +0700

    percobaan kecil

---
+++ static/js/home.js
@@ -0,0 +1,23 @@
+var roomData = [
+    {image: "/static/images/room1.jpg"},
+    {image: "/static/images/room2.jpg"},
+    {image: "/static/images/room3.jpg"}
+];
+
+var roomImage = document.getElementById("roomImage");
+var roomInfo = document.getElementById("roomInfo");
+var statusBox = document.querySelector(".status-box");
+var statusElement = document.getElementById("status");
+var index = 0;
+
+var status = "Tertutup";
+
+function updateRoom() {
+    index = (index + 1) % roomData.length;
+    roomImage.src = roomData[index].image;
+    roomInfo.innerHTML = `<h2>Status Ruangan Embeded</h2>`;
+    statusBox.className = status === "Terbuka" ? "status-box status-open" : "status-box status-closed";
+    statusElement.textContent = status;
+}
+
+setInterval(updateRoom, 3000);
