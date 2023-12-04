f70643cf70f5151e3c756017c546e51069314fdc
RafiRashya <rafi.rashya.22@gmail.com>
2023-12-04 12:55:23 +0700

    session limit

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, make_response
+from flask import Flask, render_template, request, redirect, url_for, session, flash, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_login'
@@ -11,7 +13,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique = True, nullable=False)
     nama = db.Column(db.String(50), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
@@ -23,9 +25,15 @@ def home():
 @app.route('/admin')
 def admin():
     if 'logged_in' in session:
-        response = make_response(render_template('adminpage.html'))
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-        return response
+        if 'login_time' in session and (datetime.now(pytz.utc) - session['login_time']).seconds < 900:
+            response = make_response(render_template('adminpage.html'))
+            response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+            return response
+        else:
+            flash('Session anda telah berakhir, silahkan login kembali', 'danger')
+            session.pop('logged_in', None)
+            session.pop('username', None)
+            session.pop('login_time', None)
     flash('Login terlebih dahulu', 'danger')
     return redirect(url_for('login'))
 
@@ -39,7 +47,7 @@ def registrasi():
         password = request.form["password"]
         confirm_password = request.form['confirm-password']
         
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(email=email, username=username).first()
         if user is None and password == confirm_password:
             new_user = User(email=email, nama=nama, username=username, password=generate_password_hash(password))
             db.session.add(new_user)
@@ -47,10 +55,10 @@ def registrasi():
             flash("Registrasi Berhasil, Silahkan Log in", 'success')
             return redirect(url_for('login'))
         elif password != confirm_password:
-             flash("Password dan konfirmasi password tidak cocok", 'error')
+             flash("Password dan konfirmasi password tidak cocok", 'danger')
              return redirect(url_for('registrasi'))
         else:
-            flash("Username sudah ada", "danger")
+            flash("Username atau Email sudah ada", "danger")
     return render_template('registerpage.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -68,6 +76,7 @@ def login():
         else:
             session['logged_in'] = True
             session['username'] = user.username
+            session['login_time'] = datetime.now(pytz.utc)
             return redirect(url_for('admin'))
     return render_template('loginpage.html')
 
@@ -80,4 +89,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
