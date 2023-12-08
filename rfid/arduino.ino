1ace3106e3d27954d524a2a001fa09b30519abf2
ultearsativa <121283150+ultearsativa@users.noreply.github.com>
2023-12-08 23:09:13 +0700

    ESP32 only

--- rfid/arduino.ino
+++ rfid/arduino.ino
@@ -2,16 +2,25 @@
 #include <Adafruit_PN532.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <LiquidCrystal_I2C.h>
 
 #define SDA_PIN 4
 #define SCL_PIN 5
 
+Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
+
 #define WIFI_SSID "Rayhan"  // Ganti dengan SSID jaringan WiFi Anda
 #define WIFI_PASSWORD "kamciper1"  // Ganti dengan kata sandi jaringan WiFi Anda
-
 const char *serverAddress = "http://192.168.18.11/rfid/process.php";  // Ganti dengan URL skrip PHP Anda
 
-Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
+uint8_t allowedUID[] = {4, 107, 67, 178, 224, 75, 128};
+
+int relayPin = 18;
+int buzzerPin = 19;
+int redLedPin = 22;
+int yellowLedPin = 23;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Alamat I2C LCD dan dimensi
 
 void setup(void) {
   Serial.begin(115200);
@@ -27,7 +36,6 @@ void setup(void) {
   }
 
   nfc.SAMConfig();
-  Serial.println("Menunggu kartu NFC ...");
 
   // Koneksi ke jaringan WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -36,6 +44,22 @@ void setup(void) {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi network");
+  Serial.println("Menunggu kartu NFC ...");
+
+  pinMode(relayPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  
+  digitalWrite(relayPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(yellowLedPin, LOW);
+
+  lcd.init(); // Inisialisasi LCD
+  lcd.backlight(); // Aktifkan backlight LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Waiting for card");
 }
 
 void loop() {
@@ -46,26 +70,67 @@ void loop() {
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
   if (success) {
-    Serial.println("Kartu NFC ditemukan!");
-
+    lcd.clear(); // Bersihkan layar LCD
+    lcd.setCursor(0, 0);
+    lcd.print("Found an NFC card!");
+    
+    Serial.println("Found an NFC card!");
     Serial.print("UID: ");
     for (int i = 0; i < uidLength; i++) {
-      Serial.print("0x");
-      Serial.print(uid[i], HEX);
+      Serial.print(uid[i]);
       Serial.print(" ");
     }
     Serial.println();
-
-    // Convert UID bytes to a string representation
-    String uidString = "";
-    for (int i = 0; i < uidLength; i++) {
-      if (uid[i] < 0x10) uidString += "0";
-      uidString += String(uid[i], HEX);
-    }
     
-    sendToServer(uidString);
-    delay(1000); // Jeda sejenak sebelum membaca kartu berikutnya
+    bool accessGranted = true;
+    if (uidLength == sizeof(allowedUID)) {
+      for (int i = 0; i < uidLength; i++) {
+        if (uid[i] != allowedUID[i]) {
+          accessGranted = false;
+          break;
+        }
+      }
+    } else {
+      accessGranted = false;
+    }
+
+    String uidString = convertUIDToString(uid, uidLength);
+    if (uidString.equalsIgnoreCase("046b43b2e04b80")) {
+      digitalWrite(relayPin, LOW);
+      digitalWrite(yellowLedPin, HIGH);
+      lcd.setCursor(0, 1);
+      lcd.print("Access granted");
+      delay(2000);
+      digitalWrite(relayPin, HIGH);
+      digitalWrite(yellowLedPin, LOW);
+
+      String uidString = convertUIDToString(uid, uidLength);
+      sendToServer(uidString);
+    } else {
+      digitalWrite(redLedPin, HIGH);
+      digitalWrite(buzzerPin, HIGH);
+      lcd.setCursor(0, 1);
+      lcd.print("Access denied");
+      delay(2000);
+      digitalWrite(redLedPin, LOW);
+      digitalWrite(buzzerPin, LOW);
+
+      String uidString = convertUIDToString(uid, uidLength);
+      sendToServer(uidString);
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Waiting for card");
   }
+}
+
+String convertUIDToString(uint8_t *uid, uint8_t uidLength) {
+  String uidString = "";
+  for (int i = 0; i < uidLength; i++) {
+    if (uid[i] < 0x10) uidString += "0";
+    uidString += String(uid[i], HEX);
+  }
+  return uidString;
 }
 
 void sendToServer(String uidString) {
--- rfid/process.php
+++ rfid/process.php
@@ -3,13 +3,21 @@ include "koneksi.php";
 ini_set('date.timezone', 'Asia/Jakarta');
 
 $now = new DateTime();
-
 $datenow = $now->format("Y-m-d H:i:s");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['uid'])) {
     $uid = $_POST['uid'];
+    $access = '';
 
-    // Query untuk menyimpan UID dan waktu ke dalam tabel t_rfid
-    $query = "INSERT INTO t_rfid (no_rfid, waktu) VALUES ('$uid', '$datenow')";
+    // Cek UID untuk menentukan nilai kolom access
+    if ($uid === '046b43b2e04b80') {
+        $access = 'GRANTED';
+    } else {
+        $access = 'DENIED';
+    }
+
+    // Query untuk menyimpan UID, waktu, dan access ke dalam tabel log_rfid
+    $query = "INSERT INTO log_rfid (no_rfid, waktu, access) VALUES ('$uid', '$datenow', '$access')";
     $result = mysqli_query($id_mysql, $query);
 
     if ($result) {
@@ -20,9 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['uid'])) {
     }
 }
 
-// Query untuk mengambil data dari tabel t_rfid
-$query_select = "SELECT  no_rfid, DATE_FORMAT(waktu, '%Y-%m-%d %H:%i:%s') as waktu_formatted FROM t_rfid";
+// Query untuk mengambil data dari tabel log_rfid
+$query_select = "SELECT no_rfid, DATE_FORMAT(waktu, '%Y-%m-%d %H:%i:%s') as waktu_formatted, access FROM log_rfid";
 
-// Eksekusi query untuk menampilkan data tabel t_rfid
+// Eksekusi query untuk menampilkan data tabel log_rfid
 $result_select = mysqli_query($id_mysql, $query_select);
 ?>
