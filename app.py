1d9d8fe3fac78d45204cb9edabd38dfea17b2328
RafiRashya <rafi.rashya.22@gmail.com>
2024-01-15 16:01:36 +0700

    Account Registration now on admin, Admin can manage Room schedule, etc

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.urandom(24)
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
@@ -33,8 +33,7 @@ class DatabaseRfid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     no_rfid = db.Column(db.String(50), nullable=False)
     waktu = db.Column(db.DateTime,nullable=False)
-    # timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique = True, nullable=False)
@@ -47,6 +46,15 @@ class Uid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.String(50), unique=True, nullable=False)
     nama = db.Column(db.String(100), nullable=False)
+
+class Schedule(db.Model):
+    __tablename__ = 'tb_roomschedules'
+
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(10), nullable=False)
+    time = db.Column(db.String(15), nullable=False)
+    subject = db.Column(db.String(50), nullable=False)
+    class_name = db.Column(db.String(30), nullable=False)
 
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
@@ -70,20 +78,30 @@ def check_session(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'logged_in' in session:
-            if 'login_time' in session and (datetime.now(pytz.utc) - session['login_time']).seconds < 900:
-                response = make_response(f(*args, **kwargs))
-                response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-                return response
-            else:
-                flash('Your session has ended, please log in again.', 'danger')
-                session.pop('logged_in', None)
-                session.pop('username', None)
-                session.pop('login_time', None)
+            # Jika sesi telah berakhir (lebih dari 15 menit), arahkan ke halaman login
+            if 'login_time' in session:
+                elapsed_time = (datetime.now(pytz.utc) - session['login_time']).seconds
+                if elapsed_time >= 900:
+                    # ... (kode lain untuk menghandle sesi yang telah berakhir)
+                    flash('Your session has ended, please log in again.', 'danger')
+                    session.pop('logged_in', None)
+                    session.pop('username', None)
+                    session.pop('login_time', None)
+                    return redirect(url_for('login'))
+                else:
+                    # Perbarui waktu login hanya jika sisa waktu kurang dari 900 detik
+                    if elapsed_time < 900:
+                        session['login_time'] = datetime.now(pytz.utc)
+                        session.permanent = True
+                        app.permanent_session_lifetime = timedelta(seconds=900 - elapsed_time)
+            response = make_response(f(*args, **kwargs))
+            response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+            return response
         flash('Please login first', 'danger')
         return redirect(url_for('login'))
     return decorated_function
 
-database = {'last_uid': 'Scan your card...'}
+database = {'last_uid': None}
 
 @app.route('/dashboard/room-info/')
 @check_session
@@ -94,13 +112,8 @@ def room_info():
 @app.route('/dashboard/room-schedule/')
 @check_session
 def schedule():
-    return render_template('schedule.html')
-
-# Route untuk contact admin
-@app.route('/dashboard/contact-admin/')
-@check_session
-def contact_admin():
-    return render_template('contact_admin.html')
+    schedules = Schedule.query.all()
+    return render_template('schedule.html', schedules=schedules)
 
 @app.route('/dashboard')
 @check_session
@@ -112,12 +125,32 @@ def home():
 def admin():
     return render_template('adminpage.html')
 
+@app.route('/admin/manage-room-schedule')
+@check_session
+def manage_schedule():
+    schedules = Schedule.query.all()
+    return render_template('manage_schedule.html', schedules=schedules)
+
+@app.route('/store_uid', methods=['POST'])
+def store_uid():
+    if request.method == 'POST' and 'uid' in request.form:
+        uid = request.form['uid']
+        waktu_sekarang = datetime.now()
+
+        try:
+            new_log = DatabaseRfid(no_rfid=uid, waktu=waktu_sekarang)
+            db.session.add(new_log)
+            db.session.commit()
+            return "Data UID berhasil disimpan ke database.\n" \
+                   "=================================================="
+        except Exception as e:
+            return f"Gagal menyimpan data UID ke database: {str(e)}"
+    return "Invalid Request"
+
 @app.route('/admin/access_log')
 def access_log():
     # Fetch log access data from the DatabaseRfid table
-    log_access_data = DatabaseRfid.query.filter(
-        DatabaseRfid.waktu >= (datetime.utcnow() - timedelta(hours=24))
-    ).all()
+    log_access_data = DatabaseRfid.query.all()
 
     # Convert the data to a list of dictionaries
     data = [
@@ -133,7 +166,102 @@ def access_log():
     # Return the data as JSON
     return jsonify(data)
 
-@app.route('/registrasi', methods=['GET', 'POST'])
+@app.route('/admin/manage_uid')
+@check_session
+def manage_uid():
+    uid_list = Uid.query.all()
+    return render_template('manage_uid.html', uid_list=uid_list)
+    
+@app.route('/admin/delete_uid/<int:uid_id>', methods=['POST'])
+@check_session
+def delete_uid(uid_id):
+    uid_to_delete = Uid.query.get(uid_id)
+    if uid_to_delete:
+        db.session.delete(uid_to_delete)
+        db.session.commit()
+        flash('UID has been deleted successfully.', 'success')
+    else:
+        flash('UID not found.', 'danger')
+    return redirect(url_for('manage_uid'))
+
+@app.route('/admin/edit_uid/<int:uid_id>', methods=['GET', 'POST'])
+@check_session
+def edit_uid(uid_id):
+    if request.method == 'GET':
+        uid_to_edit = Uid.query.get(uid_id)
+        if uid_to_edit:
+            return render_template('edit_uid.html', uid_to_edit=uid_to_edit)
+        else:
+            flash('UID not found.', 'danger')
+            return redirect(url_for('manage_uid'))
+    elif request.method == 'POST':
+        nama_baru = request.form.get('nama')
+        uid_to_update = Uid.query.get(uid_id)
+        if uid_to_update:
+            uid_to_update.nama = nama_baru
+            db.session.commit()
+            flash('UID has been updated successfully.', 'success')
+            return redirect(url_for('manage_uid'))
+        else:
+            flash('UID not found.', 'danger')
+            return redirect(url_for('manage_uid'))
+
+@app.route('/add_room_schedule', methods=['POST'])
+def add_room_schedule():
+    if request.method == 'POST':
+        day = request.form.get('day')
+        time = request.form.get('time')
+        subject = request.form.get('subject')
+        class_name = request.form.get('class_name')
+
+        new_schedule = Schedule(day=day, time=time, subject=subject, class_name=class_name)
+        db.session.add(new_schedule)
+        db.session.commit()
+        flash("Room schedule added.", "success")
+    return redirect(url_for('manage_schedule'))
+
+@app.route('/admin/edit_schedule/<int:schedule_id>', methods=['GET', 'POST'])
+@check_session
+def edit_schedule(schedule_id):
+    if request.method == 'GET':
+        schedule_to_edit = Schedule.query.get(schedule_id)
+        if schedule_to_edit:
+            return render_template('edit_schedule.html', schedule_to_edit=schedule_to_edit)
+        else:
+            flash('Schedule not found.', 'danger')
+            return redirect(url_for('manage_schedule'))
+    elif request.method == 'POST':
+        new_day = request.form.get('day')
+        new_time = request.form.get('time')
+        new_subject = request.form.get('subject')
+        new_class_name = request.form.get('class_name')
+        schedule_to_update = Schedule.query.get(schedule_id)
+        if schedule_to_update:
+            schedule_to_update.day = new_day
+            schedule_to_update.time = new_time
+            schedule_to_update.subject = new_subject
+            schedule_to_update.class_name = new_class_name
+            db.session.commit()
+            flash('Schedule has been updated successfully.', 'success')
+            return redirect(url_for('manage_schedule'))
+        else:
+            flash('Schedule not found.', 'danger')
+            return redirect(url_for('manage_schedule'))
+
+@app.route('/admin/delete_schedule/<int:schedule_id>', methods=['POST'])
+@check_session
+def delete_schedule(schedule_id):
+    schedule_to_delete = Schedule.query.get(schedule_id)
+    if schedule_to_delete:
+        db.session.delete(schedule_to_delete)
+        db.session.commit()
+        flash('Schedule has been deleted successfully.', 'success')
+    else:
+        flash('Schedule not found.', 'danger')
+    return redirect(url_for('manage_schedule'))
+
+@app.route('/admin/account-registration', methods=['GET', 'POST'])
+@check_session
 def registrasi():
     if request.method == 'POST':
         email = request.form['email']
@@ -141,6 +269,7 @@ def registrasi():
         username = request.form["username"]
         password = request.form["password"]
         confirm_password = request.form['confirm_password']
+        role = request.form['role']
         
         if (
             len(password) < 8
@@ -153,11 +282,11 @@ def registrasi():
 
         user = User.query.filter((User.email==email) | (User.username==username)).first()
         if user is None and password == confirm_password:
-            new_user = User(email=email, nama=nama, username=username, password=generate_password_hash(password), role='user')
+            new_user = User(email=email, nama=nama, username=username, password=generate_password_hash(password), role=role)
             db.session.add(new_user)
             db.session.commit()
-            flash("Registration succesful, please log in.", 'success')
-            return redirect(url_for('login'))
+            flash("Account successfully registered", 'success')
+            return redirect(url_for('admin'))
         elif password != confirm_password:
              flash("Passwords do not match", 'danger')
              return redirect(url_for('registrasi'))
@@ -175,8 +304,9 @@ def login():
 
         if user is None:
             flash("User Didn't Find", 'danger')
+            return redirect(url_for('admin'))
         elif not check_password_hash(user.password, password):
-            flash('Login failed. Please double-check your username/email and password', 'danger')
+            flash('Login failed. Incorrect username or password', 'danger')
         else:  # User found and password matches
             session['logged_in'] = True 
             session['username'] = user.username
@@ -224,6 +354,15 @@ def reset_password(token):
         new_password = request.form.get('new_password')
         confirm_password = request.form.get('confirm_password')
 
+        if (
+            len(new_password) < 8
+            or not any(c.islower() for c in new_password)   
+            or not any(c.isupper() for c in new_password)
+            or not any(c.isdigit() for c in new_password)
+        ):
+            flash('Your password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, and one digit.')
+            return redirect(url_for('reset_password', token=token))
+
         # Validasi kata sandi
         if not new_password or not confirm_password:
             flash('Password cannot be empty', 'danger')
@@ -254,7 +393,6 @@ def nfc_scan():
 
         # Update UID terakhir di database
         database['last_uid'] = uid
-
         response = {'status': 'success', 'message': f'UID {uid} diterima'}
         return jsonify(response)
 
@@ -267,21 +405,24 @@ def nfc_scan():
 @check_session
 def regis_uid():
     if request.method == 'POST':
-        uid= database['last_uid']
+        uid = database['last_uid']
         nama = request.form['nama']
-        uid = Uid.query.filter_by(uid=uid)
-        if uid:
-            flash('The UID already exist', 'warning')
+        uid_record = Uid.query.filter_by(uid=uid).first()
+        if uid_record:
+            flash('The UID already exists', 'warning')
             return redirect(url_for('regis_uid'))
         else:
             new_uid = Uid(uid=uid, nama=nama)
             db.session.add(new_uid)
             db.session.commit()
-            flash('Your card succesfully registered!')
+            flash('Your card successfully registered!')
             return redirect(url_for('regis_uid'))
-    return render_template('registrasi_uid.html', last_uid=database['last_uid'])
+
+    # Mendapatkan nilai last_uid dari database atau sesuai kebutuhan aplikasi
+    last_uid = database['last_uid']
+    return render_template('registrasi_uid.html', last_uid=last_uid)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
